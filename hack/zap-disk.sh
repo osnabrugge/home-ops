f0c4f8fb94d7896f4e8995127d90ffc8e2ba8293
osnabrugge <sean@seanv.com>
2023-03-28 22:10:46 -0400

    add shell scripts

---
+++ hack/zap-disk.sh
@@ -0,0 +1,13 @@
+DISK="/dev/sdb"
+
+# Zap the disk to a fresh, usable state (zap-all is important, b/c MBR has to be clean)
+sgdisk --zap-all $DISK
+
+# Wipe a large portion of the beginning of the disk to remove more LVM metadata that may be present
+dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
+
+# SSDs may be better cleaned with blkdiscard instead of dd
+blkdiscard $DISK
+
+# Inform the OS of partition table changes
+partprobe $DISK
