d72c96a95f4d85ded18ecb3921744409ad4c812b
osnabrugge <sean@seanv.com>
2023-05-14 05:14:43 -0400

    deploy to test

--- hack/delete-all-stuck-pods.sh
+++ hack/delete-all-stuck-pods.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl get po | grep -v Running | awk 'NR>1 {print $1}' | xargs kubectl delete po --force --grace-period=0
+kubectl get pods --all-namespaces | grep Terminating | while read line; do
+pod_name=$(echo $line | awk '{print $2}' ) \
+name_space=$(echo $line | awk '{print $1}' ); \
+kubectl delete pods $pod_name -n $name_space --grace-period=0 --force; \
+done
