cf14a56dc366aa19c9f43edf861833bfb3bd71a2
osnabrugge <sean@seanv.com>
2023-05-15 07:13:33 -0400

    attempt to deploy w/o helm disablewait

--- hack/delete-all-stuck-pods.sh
+++ hack/delete-all-stuck-pods.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-function delete_stuckpods () {
-    echo "Deleting stuck pods in namespace $1"
-    kubectl get pods -n $1 | grep Evicted | awk '{print $1}' | xargs kubectl delete pod -n $1
-}
+kubectl get pod --all-namespaces | awk '{if ($4 != "Running") system ("kubectl -n " $1 " delete pods " $2  " --grace-period=0 " " --force ")}'
 
-STUCK_NS=$(kubectl get ns | awk '$2=="Evicted" {print $1}')
-
-for ns in $STUCK_NS
-do
-    delete_stuckpods $ns
-done
