9ae506eb420caef478e99b2f0a7f037c3fd0d449
osnabrugge <sean@seanv.com>
2023-05-14 23:33:34 -0400

    enable host network

--- hack/delete-all-stuck-pods.sh
+++ hack/delete-all-stuck-pods.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-kubectl get pods --all-namespaces | grep Terminating | while read line; do
-pod_name=$(echo $line | awk '{print $2}' ) \
-name_space=$(echo $line | awk '{print $1}' ); \
-kubectl delete pods $pod_name -n $name_space --grace-period=0 --force; \
+function delete_stuckpods () {
+    echo "Deleting stuck pods in namespace $1"
+    kubectl get pods -n $1 | grep Evicted | awk '{print $1}' | xargs kubectl delete pod -n $1
+}
+
+STUCK_NS=$(kubectl get ns | awk '$2=="Evicted" {print $1}')
+
+for ns in $STUCK_NS
+do
+    delete_stuckpods $ns
 done
