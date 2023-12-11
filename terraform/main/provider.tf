94e0d678c0f0a1cf1ae041c75787911690b3f907
osnabrugge <18647660+osnabrugge@users.noreply.github.com>
2023-12-11 09:29:18 -0500

    feat: overhaul to new build

---
+++ terraform/main/provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.84.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.46.0"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.20.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+provider "cloudflare" {
+  # Configuration options
+}
